080bd767836081707096cbdee451f834c5804aeb
GerardoBrescia <gerrybrescia99@gmail.com>
2023-08-14 18:33:22 +0200

    added num_outputs metric

--- terraformmetrics/import_metrics.py
+++ terraformmetrics/import_metrics.py
@@ -19,6 +19,7 @@ from terraformmetrics.terraformspec.num_error_handling import NumErrorHandling
 from terraformmetrics.terraformspec.num_regex import NumRegex
 from terraformmetrics.terraformspec.num_path import NumPath
 from terraformmetrics.terraformspec.num_var import NumVar
+from terraformmetrics.terraformspec.num_output import NumOutputs
 
 general_metrics = {
                     'lines_code': LinesCode,
@@ -41,5 +42,6 @@ tf_metrics = {
                 'num_regex': NumRegex,
                 'num_path': NumPath,
                 'num_var': NumVar,
+                'num_outputs': NumOutputs,
              }
 
